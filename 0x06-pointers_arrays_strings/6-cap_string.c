0b560f81e2362c61ddae34ed08c2fd913fa5aeec
yiadomdk.com <yiadomdk@gmail.com>
2022-04-25 16:15:10 -0700

    String and array fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,32 +5,32 @@
  *Return: void
  */
 char *cap_string(char *s)
+{ 
+int index = 0;
+
+while (s[index])
 {
-	 int index = 0;
+while (!(s[index] >= 'a' && s[index] <= 'z'))
+index++;
 
-	 while (s[index])
-	 {
-		 while (!(s[index] >= 'a' && s[index] <= 'z'))
-			 index++;
+if (s[index - 1] == ' ' ||
+s[index - 1] == '\t' ||
+s[index - 1] == '\n' ||
+s[index - 1] == ',' ||
+s[index - 1] == ';' ||
+s[index - 1] == '.' ||
+s[index - 1] == '!' ||
+s[index - 1] == '?' ||
+s[index - 1] == '"' ||
+s[index - 1] == '(' ||
+s[index - 1] == ')' ||
+s[index - 1] == '{' ||
+s[index - 1] == '}' ||
+index == 0)
+s[index] -= 32;
 
-		 if (s[index - 1] == ' ' ||
-		 s[index - 1] == '\t' ||
-		 s[index - 1] == '\n' ||
-		 s[index - 1] == ',' ||
-		 s[index - 1] == ';' ||
-		 s[index - 1] == '.' ||
-		 s[index - 1] == '!' ||
-		 s[index - 1] == '?' ||
-		 s[index - 1] == '"' ||
-		 s[index - 1] == '(' ||
-		 s[index - 1] == ')' ||
-		 s[index - 1] == '{' ||
-		 s[index - 1] == '}' ||
-		 index == 0)
-		 s[index] -= 32;
+index++;
+}
 
-		 index++;
-	 }
-
-	 return (s);
+return (s);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,23 +6,23 @@
  *
  * Return: the resulting string;
  */
-
 char *leet(char *s)
 {
-       	int i, j;
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
+int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (s[i] == a[j])
-			{
-				s[i] = b[j];
-			}
-		}
-	}
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
 
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+}
+}
+}
+
+return (s);
 }
